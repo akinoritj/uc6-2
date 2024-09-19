50e654990e5169d27670568dbe998954dd71a0ff
Mauricio de Oliveira e Silva <Mauricio50500546@edu.df.senac.br>
2024-09-19 12:02:12 -0300

    Tratamentos e Diagnósticos

---
+++ Assets/Scripts/EscolhaPacientes.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class EscolhaPacientes : MonoBehaviour
+{
+    public Button paciente1;
+    public Button paciente2;
+    public Button paciente3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
